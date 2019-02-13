4ed3d12fa3b109b18e4b424c46ed90b4eb0adc9c
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-02-12 20:06:46 -0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -130,7 +130,53 @@ if [ ! -f "$WG_CONFIG" ]; then
         apt install unbound unbound-host -y
         curl -o /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
         rm /etc/unbound/unbound.conf
-        curl -o /etc/unbound/unbound.conf https://gist.githubusercontent.com/LiveChief/06df7e7368df150143dca624dbad7d8f/raw/a84368fe43983f6f521fd8ece23e10cdb1a03303/WireGuard-Unbound.conf
+        echo "server:
+
+  num-threads: 4
+
+  #Enable logs
+  verbosity: 1
+
+  #list of Root DNS Server
+  root-hints: "/var/lib/unbound/root.hints"
+
+  #Use the root servers key for DNSSEC
+  auto-trust-anchor-file: "/var/lib/unbound/root.key"
+
+  #Respond to DNS requests on all interfaces
+  interface: 0.0.0.0
+  max-udp-size: 3072
+
+  #Authorized IPs to access the DNS Server
+  access-control: 0.0.0.0/0                 refuse
+  access-control: 127.0.0.1                 allow
+  access-control: 10.8.0.0/24         allow
+
+  #not allowed to be returned for public internet  names
+  private-address: 10.8.0.0/24
+
+  # Hide DNS Server info
+  hide-identity: yes
+  hide-version: yes
+
+  #Limit DNS Fraud and use DNSSEC
+  harden-glue: yes
+  harden-dnssec-stripped: yes
+  harden-referral-path: yes
+
+  #Add an unwanted reply threshold to clean the cache and avoid when possible a DNS Poisoning
+  unwanted-reply-threshold: 10000000
+
+  #Have the validator print validation failures to the log.
+  val-log-level: 1
+
+  #Minimum lifetime of cache entries in seconds
+  cache-min-ttl: 1800
+
+  #Maximum lifetime of cached entries
+  cache-max-ttl: 14400
+  prefetch: yes
+  prefetch-key: yes" > /etc/unbound/unbound.conf
         chown -R unbound:unbound /var/lib/unbound
         systemctl enable unbound
     elif [ "$DISTRO" == "Debian" ]; then
